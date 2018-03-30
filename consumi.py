b37a6214f715afcbcaadd3c595b01885563383ab
sergio <sergio.cordedda@abinsula.com>
2018-03-30 16:34:53 +0200

    Aggiunto metodo per consumi medi

--- consumi.py
+++ consumi.py
@@ -2,22 +2,31 @@
 import RPi.GPIO as GPIO
 import time
 import pigpio
+import threading
 
 INPUT_PIN = 11 #numero fisico sulla board
 timeStarted = False
+numeroLampeggi = 0
 
 
 #callback chiamata dal lampeggio del led
 def manageTime():
-	global timeStarted
-	
-	if timeStarted == False:
-		start = time.time()
-		timeStarted = True
-	else:
-		timeBetweenTwoflash = time.time() - start #
-		consumoWatt = 3600 / timeBetweenTwoflash
-		return consumoWatt
+	try:
+		global timeStarted
+		global numeroLampeggi
+
+		numeroLampeggi += 1
+		
+		if timeStarted == False:
+			start = time.time()
+			timeStarted = True
+		else:
+			timeBetweenTwoflash = time.time() - start #
+			consumoWatt = 3600 / timeBetweenTwoflash
+			return consumoWatt
+
+	except Exception, e:
+		print('Exception in manageTime method: ',e)
 
 
 
@@ -36,14 +45,25 @@ def realTime():
 
 
 def consumoMedio():
+	try:
+		global numeroLampeggi
+
+		threading.Timer(3600.0, timer).start()
+
+	except Exception,e:
+		print('Errore nel conteggio del numero dei lampeggi: ', e)
 
 
 
 
+#MAIN
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	realTime()
+
+	realTime() #controllo consumi real time
+
+	consumoMedio() #controllo dei consumi medi (E' un thread asincrono)
 
 
 
